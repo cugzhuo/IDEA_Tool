8d94b6a7f2f0d6a803b8737e88552240b3cf72e4
GitHub Action <action@github.com>
2022-01-04 02:47:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/cugzhuo/ideatool/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cugzhuo/ideatool/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cugzhuo.ideatool.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cugzhuo.ideatool.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/cugzhuo/ideatool/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cugzhuo/ideatool/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.cugzhuo.ideatool.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cugzhuo.ideatool.MyBundle
 
 class MyProjectService(project: Project) {
 
